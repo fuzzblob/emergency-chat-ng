decaf49a8e5640ac3b5f7064be966fffc4033a94
fuzzblob <admin@fuzzblob.com>
2025-06-18 04:22:09 +0200

    fix followup chat setup crash

--- app/src/main/java/com/sephdebusser/aspiemeltdown/SwitchHelperICS.java
+++ app/src/main/java/com/sephdebusser/aspiemeltdown/SwitchHelperICS.java
@@ -1,17 +1,18 @@
 package com.sephdebusser.aspiemeltdown;
 
 import android.app.Activity;
-import android.widget.Switch;
+
+import androidx.appcompat.widget.SwitchCompat;
 
 /**
  * Created by Seph De Busser on 18/07/2015.
  */
 public class SwitchHelperICS extends SwitchHelper {
 
-    Switch s;
+    SwitchCompat s;
 
     public SwitchHelperICS(Activity activity) {
-        s = (Switch) activity.findViewById(R.id.userSwitch);
+        s = (SwitchCompat) activity.findViewById(R.id.userSwitch);
     }
 
     @Override
