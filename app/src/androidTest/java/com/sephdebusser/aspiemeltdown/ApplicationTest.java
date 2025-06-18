cea79aa72b5b56c5c165b12bbb9fb90a9a27f8a0
fuzzblob <admin@fuzzblob.com>
2025-06-18 03:01:48 +0200

    fix test compilation

--- app/src/androidTest/java/com/sephdebusser/aspiemeltdown/ApplicationTest.java
+++ app/src/androidTest/java/com/sephdebusser/aspiemeltdown/ApplicationTest.java
@@ -1,13 +1,25 @@
 package com.sephdebusser.aspiemeltdown;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNotNull;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * <a href="https://developer.android.com/studio/test">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
+    public void testApplicationNotNull() {
+        // Get the application context
+        Application app = ApplicationProvider.getApplicationContext();
+        // Assert that the application is not null
+        assertNotNull(app);
     }
 }
